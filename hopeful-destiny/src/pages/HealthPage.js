7317cb918338cc5e46af61893c4b0a25b502cd65
Tony Mputhia <tonymmputhia@gmail.com>
2025-02-26 15:29:08 +0300

    Add health and nutrition page

---
+++ hopeful-destiny/src/pages/HealthPage.js
@@ -0,0 +1,11 @@
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+
+const ThematicPage = () => (
+    <>
+        <Navigation />
+        <Footer />
+    </>
+);
+
+export default ThematicPage;
