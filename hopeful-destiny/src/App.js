5bb810ef1d7d6aebeff6fe9d2e172b76277e3291
Tony Mputhia <tonymmputhia@gmail.com>
2025-03-20 07:18:09 +0300

    Add routes to the different pages of the application.

--- hopeful-destiny/src/App.js
+++ hopeful-destiny/src/App.js
@@ -1,12 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ClimatePage from "./pages/ClimatePage";
+import EducationPage from "./pages/EducationPage";
+import FoodSecurityPage from "./pages/FoodSecurityPage";
+import HealthPage from "./pages/HealthPage";
+import WaterPage from "./pages/WaterPage";
 import React from 'react';
 import HomePage from "./pages/HomePage";
 import "./index.css";  // Ensure styles are applied
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router basename="/Hopeful-Destiny">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/climate" element={<ClimatePage />} />
+        <Route path="/education" element={<EducationPage />} />
+        <Route path="/food-security" element={<FoodSecurityPage />} />
+        <Route path="/health" element={<HealthPage />} />
+        <Route path="/water-sanitation" element={<WaterPage />} />
+      </Routes>
+    </Router>
+      
   );
 }
 
