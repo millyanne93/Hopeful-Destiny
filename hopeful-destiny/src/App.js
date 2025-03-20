3b71c0e0d3b28e7ba4036cc58328925856f368dc
millyanne93 <nmillyanne20@gmail.com>
2025-03-20 11:03:42 +0300

    fix conflict in App.js.

--- hopeful-destiny/src/App.js
+++ hopeful-destiny/src/App.js
@@ -1,22 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ClimatePage from "./pages/ClimatePage";
+import EducationPage from "./pages/EducationPage";
+import FoodSecurityPage from "./pages/FoodSecurityPage";
+import HealthPage from "./pages/HealthPage";
+import WaterPage from "./pages/WaterPage";
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
-import Navigation from "./components/Navigation";
-import "./index.css";
+import "./index.css";  // Ensure styles are applied
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+    <Router basename="/Hopeful-Destiny">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/climate" element={<ClimatePage />} />
+        <Route path="/education" element={<EducationPage />} />
+        <Route path="/food-security" element={<FoodSecurityPage />} />
+        <Route path="/health" element={<HealthPage />} />
+        <Route path="/water-sanitation" element={<WaterPage />} />
+      </Routes>
     </Router>
+      
   );
 }
-
-export default App;
